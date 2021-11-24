3f428a916f50f61a4680d5729260033c3b8b9363
SergeyLebidko <it.sergeyler@mail.ru>
2021-11-24 14:02:09 +0300

    Добавлены кнопки управления окном. Реализована функциональность скрытия окна

--- js/control.js
+++ js/control.js
@@ -1,3 +1,19 @@
 const startButton = document.getElementById('start-button');
 
-startButton.addEventListener('click', () => console.log('Клик по кнопке!'));
+const frame = document.getElementById('frame');
+const frameToHiddenBtn = document.getElementById('frame-to-hidden-btn');
+const frameToNormalBtn = document.getElementById('frame-to-normal-btn');
+const frameToTurnBtn = document.getElementById('frame-to-turn-btn');
+
+// При клике на стартовую кнопку - убираем её и показываем псевдобраузер
+startButton.addEventListener('click', () => {
+    startButton.classList.add('start-button_hidden');
+    frame.classList.remove('frame_hidden');
+});
+
+// При клике на кнопку закрытия псевдобраузера (красную) - убираем его и показываем стартовую кнопку
+frameToHiddenBtn.addEventListener('click', () => {
+    startButton.classList.remove('start-button_hidden');
+    frame.classList.add('frame_hidden');
+});
+
